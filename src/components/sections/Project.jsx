1fd0cc0f93ffb3e8975bebe7e3c0afbb676bf3cf
farisfian06 <farisfian06@gmail.com>
2024-12-16 23:01:24 +0700

    Update Project.jsx

--- src/components/sections/Project.jsx
+++ src/components/sections/Project.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
+import ProjectCard from "../ProjectCard";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Project = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const scroll = useTransform(scrollYProgress, [0.3, 1], ["1%", "-100%"]);
   return (
     <section>
       <div className="container">
-        <h2>Projects</h2>
+        <div ref={ref} className="h-[200vh] relative">
+          <div className="sticky top-0 h-screen overflow-hidden">
+            <h2 className="font-primaryBold text-[4svh]">Projects</h2>
+            <motion.div
+              style={{
+                x: scroll,
+              }}
+              className="flex gap-4 w-full flex-nowrap"
+            >
+              <ProjectCard />
+              <ProjectCard />
+              <ProjectCard />
+              <ProjectCard />
+              <ProjectCard />
+              <ProjectCard />
+            </motion.div>
+          </div>
+        </div>
       </div>
     </section>
   );
