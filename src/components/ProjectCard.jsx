09be1ebfdcd36a1a95b05aebdfb62630f09a1226
farisfian06 <farisfian06@gmail.com>
2024-12-20 02:46:50 +0700

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,30 +1,30 @@
-import React from "react";
+import { useInView } from "framer-motion";
+import React, { useEffect, useRef } from "react";
 
-const ProjectCard = ({ prjImg }) => {
+const ProjectCard = ({ prjImg, keterangan, judul, onInView }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { margin: "0px -50% 0px -50%" });
+
+  useEffect(() => {
+    if (isInView) {
+      onInView(); // Panggil fungsi jika kartu in view
+    }
+  }, [isInView, onInView]);
   return (
-    <div className="bg-gray-800 min-w-[20rem] h-full rounded-3xl overflow-hidden">
+    <div
+      ref={ref}
+      className={`bg-gray-800 w-full h-fit rounded-3xl overflow-hidden transform transition-transform duration-300 ${
+        isInView ? "" : "scale-75"
+      }`}
+    >
       <img
-        className="w-full h-20 max-h-[25%] relative top-0"
+        className="w-full h-auto object-contain max-h-[25%] relative top-0"
         src={prjImg}
         alt="Project image"
       />
-      <div className="p-8">
-        <h3 className="text-primaryOrange-0 font-primaryRegular">
-          Judul project
-        </h3>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Exercitationem quia amet magnam voluptate nihil fuga temporibus
-          explicabo voluptates hic saepe! Qui, molestias unde voluptate sint
-          quae dignissimos enim ipsa est quo earum atque accusamus at cum non
-          quisquam a natus reprehenderit, neque asperiores ea eum? Quaerat illum
-          cumque, quo quasi delectus nobis error maxime aut placeat ex ratione
-          doloremque molestias autem dolores ut illo? Corporis repellat quis
-          maiores, quisquam quos perspiciatis qui minus tempora sapiente
-          blanditiis ad, et, omnis vel quidem distinctio! Dolor, illo. Dolor
-          laudantium velit excepturi aut et! Quos corporis beatae dolorum
-          suscipit blanditiis porro repudiandae minus cumque.
-        </p>
+      <div className="px-8 py-4">
+        <h3 className="text-primaryOrange-0 font-primaryRegular">{judul}</h3>
+        <p>{keterangan}</p>
       </div>
     </div>
   );
