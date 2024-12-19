fb1331915aef91465fcfb955c87f66f8a5c2214c
farisfian06 <farisfian06@gmail.com>
2024-12-20 02:47:02 +0700

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,18 +5,23 @@ import HeroSection from "../components/sections/HeroSection";
 import AboutMe from "../components/sections/AboutMe";
 import Experiences from "../components/sections/Experiences";
 import Project from "../components/sections/Project";
+import TechStack from "../components/sections/TechStack";
+import { ReactLenis } from "lenis/dist/lenis-react";
 
 const LandingPage = () => {
   return (
     <>
-      <Navbar />
-      <HeroSection />
-      <main>
-        <AboutMe />
-        <Experiences />
-        <Project />
-      </main>
-      <Footer />
+      <ReactLenis root>
+        <Navbar />
+        <HeroSection />
+        <main>
+          <AboutMe />
+          <Experiences />
+          <Project />
+          <TechStack />
+        </main>
+        <Footer />
+      </ReactLenis>
     </>
   );
 };
