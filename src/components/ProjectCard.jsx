e2431075ffd87141e5f1902652a9c43ba6aeda9c
farisfian06 <farisfian06@gmail.com>
2024-12-22 02:29:07 +0700

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -13,16 +13,16 @@ const ProjectCard = ({ prjImg, keterangan, judul, onInView }) => {
   return (
     <div
       ref={ref}
-      className={`bg-gray-800 w-full h-fit rounded-3xl overflow-hidden transform transition-transform duration-300 ${
+      className={`bg-gray-800 w-full sm:w-[90%] h-fit rounded-3xl overflow-hidden transform transition-transform duration-300 group ${
         isInView ? "" : "scale-75"
       }`}
     >
       <img
-        className="w-full h-auto object-contain max-h-[25%] relative top-0"
+        className="w-full object-cover sm:max-h-[12rem] relative top-0 group-hover:scale-105 transition-all duration-500 ease-in-out"
         src={prjImg}
         alt="Project image"
       />
-      <div className="px-8 py-4">
+      <div className="px-8 py-4 relative bg-gray-800">
         <h3 className="text-primaryOrange-0 font-primaryRegular">{judul}</h3>
         <p>{keterangan}</p>
       </div>
