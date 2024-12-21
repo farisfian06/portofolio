b7c0e4187b1a396c2e46f71aa8d625a58ff0b0d6
farisfian06 <farisfian06@gmail.com>
2024-12-21 20:28:08 +0700

    Update HeroSection.jsx

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -33,34 +33,33 @@ const HeroSection = () => {
   );
 
   return (
-    <header ref={targetRef} className="relative h-[150vh] ">
-      <div className="sticky top-0 pt-11">
-        <div className="container">
-          <div className="flex">
-            <div className="relative w-1/3">
-              <div className="overflow-hidden h-full relative">
+    <header>
+      <div className="container relative h-fit">
+        <div ref={targetRef} className="h-[200vh]">
+          <div className="flex h-screen flex-col sm:flex-row gap-8 sm:gap-12 sticky justify-center sm:justify-start items-center top-0 w-full">
+            <div className="relative h-1/3 sm:h-[80%] sm:w-1/3">
+              <div className="overflow-hidden h-full flex justify-center">
                 <motion.img
                   src={profileImg}
                   alt="Profile image"
-                  className="max-w-[70%] object-contain absolute bottom-0 left-5 z-10"
+                  className="w-full h-auto object-contain relative bottom-0 z-10"
                   style={{ y: y }}
                 />
               </div>
               <motion.div
-                className="w-[80%] h-full absolute top-0 rounded-md shadow-lg"
+                className="w-full h-full absolute top-0 rounded-md shadow-lg"
                 style={{
                   transform: rotasi,
                   backgroundImage: gradient,
                 }}
               ></motion.div>
             </div>
-            <div className="">
-              {/* Judul */}
+            <div className="judul">
               <motion.h1
                 whileInView={{ y: [100, 0], opacity: [0, 1] }}
                 viewport={{ once: true }}
                 transition={{ duration: 0.5, ease: easeOut }}
-                className="text-left font-primaryRegular tracking-wide leading-tight p-0 font-[600] text-[12svh]"
+                className="text-left font-primaryRegular tracking-wide leading-tight p-0 font-[600] text-[5rem]"
               >
                 HELLO, MY <br />
                 NAME IS <br />
@@ -71,7 +70,7 @@ const HeroSection = () => {
                 transition={{ duration: 1, ease: easeOut, delay: 0.5 }}
                 className="flex"
               >
-                <h1 className="bg-primaryOrange-0 font-SerifRegular leading-none w-fit tracking-normal font-normal text-primaryBlack-0 text-[12svh]">
+                <h1 className="bg-primaryOrange-0 font-SerifRegular leading-none w-fit tracking-normal font-normal text-primaryBlack-0 text-[5rem]">
                   FARIS
                 </h1>
                 <p className="font-SerifRegular ms-2 font-[200] text-[2svh]">
@@ -82,7 +81,7 @@ const HeroSection = () => {
                 whileInView={{ y: [100, 0], opacity: [0, 1] }}
                 viewport={{ once: true }}
                 transition={{ duration: 0.5, ease: easeOut }}
-                className="text-left font-primaryRegular tracking-wide leading-tight p-0 font-[600] text-[12svh]"
+                className="text-left font-primaryRegular tracking-wide leading-tight p-0 font-[600] text-[5rem]"
               >
                 FRONT END <br />
                 DEVELOPER.
