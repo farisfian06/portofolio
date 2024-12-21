1f2caffeebe28bb99a3ea559a1417051987738d3
farisfian06 <farisfian06@gmail.com>
2024-12-21 20:28:10 +0700

    Update Project.jsx

--- src/components/sections/Project.jsx
+++ src/components/sections/Project.jsx
@@ -15,19 +15,27 @@ import kbmdsi5 from "../../assets/project/kbmdsi 5.png";
 import kbmdsi6 from "../../assets/project/kbmdsi 6.png";
 import kbmdsi7 from "../../assets/project/kbmdsi 7.png";
 import kelanaKampus from "../../assets/project/kelanaKampus.png";
-import kelanaKampus2 from "../../assets/project/kelanaKampus.png";
-import kelanaKampus3 from "../../assets/project/kelanaKampus.png";
-import kelanaKampus4 from "../../assets/project/kelanaKampus.png";
-import kelanaKampus5 from "../../assets/project/kelanaKampus.png";
-import kelanaKampus6 from "../../assets/project/kelanaKampus.png";
-import { Link } from "react-router-dom";
+import kelanaKampus2 from "../../assets/project/kelanaKampus 2.png";
+import kelanaKampus3 from "../../assets/project/kelanaKampus 3.png";
+import kelanaKampus4 from "../../assets/project/kelanaKampus 4.png";
+import kelanaKampus5 from "../../assets/project/kelanaKampus 5.png";
+import kelanaKampus6 from "../../assets/project/kelanaKampus 6.png";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules"; // Import Navigation
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "../../styles/carousel.css";
 
 const listProject = [
   {
     id: 1,
     judul: "KBMDSI UB",
     domain: "kbmdsi.ub.ac.id",
-    img: [kbmdsi, kbmdsi2, kbmdsi3, kbmdsi4, kbmdsi5, kbmdsi6, kbmdsi7],
+    img: [kbmdsi, kbmdsi3, kbmdsi4, kbmdsi2, kbmdsi5, kbmdsi6, kbmdsi7],
     keterangan:
       "Organization profile website of the Keluarga Besar Departemen Sistem Informasi",
   },
@@ -54,10 +62,10 @@ const listProject = [
     img: [
       kelanaKampus,
       kelanaKampus2,
-      kelanaKampus3,
-      kelanaKampus4,
       kelanaKampus5,
       kelanaKampus6,
+      kelanaKampus3,
+      kelanaKampus4,
     ],
     keterangan:
       "Website that is involved in mental health and aims to help friends who have problems while living away from home",
@@ -66,7 +74,7 @@ const listProject = [
     id: 5,
     judul: "Read Mate",
     domain: "",
-    img: "https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    img: [],
     keterangan:
       "Mobile apps for reading article, with a feature like bookmarking and reading history. This app is integrated with firebase database",
   },
@@ -78,19 +86,19 @@ const Project = () => {
     target: ref,
   });
 
-  const scroll = useTransform(scrollYProgress, [0.2, 1], ["11%", "-75%"]);
+  const scroll = useTransform(scrollYProgress, [0.2, 1], ["12%", "-73%"]);
   const [activeProject, setActiveProject] = useState(null);
   return (
     <section>
       <div className="container">
         <div ref={ref} className="h-[300vh] relative">
-          <div className="sticky top-0 h-screen overflow-hidden flex flex-col">
-            <h2 className="font-primaryBold text-[4svh]">Projects</h2>
+          <div className="sticky top-0 h-screen overflow-hidden flex flex-col py-6">
+            <h2 className="font-primaryBold text-3xl">Projects</h2>
             <motion.div
               style={{
                 x: scroll,
               }}
-              className="grid grid-cols-5 w-[200vw] h-[50%] items-center justify-center gap-36"
+              className="grid grid-flow-col w-auto-cols-auto w-[400vw] sm:w-[200vw] min-h-[50%] items-center justify-center gap-2 sm:gap-36"
             >
               {listProject.map((item) => (
                 <ProjectCard
@@ -102,9 +110,9 @@ const Project = () => {
                 />
               ))}
             </motion.div>
-            <div className=" max-w-screen-md self-center h-full">
-              {activeProject ? (
-                <div className="mockup-browser border-base-300 border-4 h-full bg-slate-400">
+            <div className=" max-w-md self-center h-full">
+              {activeProject && (
+                <div className="mockup-browser border-base-300 border-4 h-fit bg-slate-400">
                   <div className="mockup-browser-toolbar">
                     <div className="input border-base-300 border">
                       <a
@@ -116,16 +124,25 @@ const Project = () => {
                       </a>
                     </div>
                   </div>
-                  <div className="border-base-300 flex bg-red-300 relative justify-center border-t-4 px-4 py-16">
-                    <img
-                      src={activeProject.img[0]}
-                      alt=""
-                      className="absolute inset-0 object-contain w-full h-auto"
-                    />
+                  <div className="border-base-300 flex relative justify-center border-t-4">
+                    <Swiper
+                      pagination={{ dynamicBullets: true }}
+                      navigation={true} // Enable navigation
+                      modules={[Pagination, Navigation]} // Include Navigation module
+                      className="w-full h-full bg-slate-500"
+                    >
+                      {activeProject.img.map((image, index) => (
+                        <SwiperSlide key={index}>
+                          <img
+                            src={image}
+                            alt={`${activeProject.judul} - Slide ${index + 1}`}
+                            className="w-full h-full object-contain"
+                          />
+                        </SwiperSlide>
+                      ))}
+                    </Swiper>
                   </div>
                 </div>
-              ) : (
-                ""
               )}
             </div>
           </div>
