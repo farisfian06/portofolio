38eb8cd53d860c9f6bea66c3517188f483c0bb66
farisfian06 <farisfian06@gmail.com>
2024-12-22 20:01:18 +0700

    Update Experiences.jsx

--- src/components/sections/Experiences.jsx
+++ src/components/sections/Experiences.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ExprienceCard from "../ExprienceCard";
 import GitHubCalendar from "react-github-calendar";
+import { motion } from "framer-motion";
 
 const experiences = [
   {
@@ -26,42 +27,43 @@ const Experiences = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   const [progressStates, setProgressStates] = useState(
-    Array(experiences.length).fill(0) // Progress setiap card mulai dari 0%
+    Array(experiences.length).fill(0)
   );
 
   const handleCardClick = (index) => {
     if (selectedIndex === index) {
-      setSelectedIndex(null); // Jika card yang aktif diklik, nonaktifkan
+      setSelectedIndex(null);
     } else {
-      setSelectedIndex(index); // Aktifkan card yang baru dipilih
+      setSelectedIndex(index);
     }
   };
 
   useEffect(() => {
     const newProgressStates = progressStates.map((progress, index) => {
       if (selectedIndex === null) {
-        return 0; // Reset semua jika tidak ada yang dipilih
+        return 0;
       } else if (index < selectedIndex) {
-        return 100; // Card sebelum card terpilih memiliki progress penuh
+        return 100;
       } else if (index === selectedIndex) {
-        return 50; // Card terpilih memiliki progress setengah
+        return 50;
       } else {
-        return 0; // Card setelah card terpilih tidak memiliki progress
+        return 0;
       }
     });
     setProgressStates(newProgressStates);
   }, [selectedIndex]);
 
   return (
-    <section>
-      <div className="container">
+    <section id="experience">
+      <div className="container py-8">
         <h2 className="font-primaryBold text-3xl text-slate-50">Experiences</h2>
         <div className="flex items-stretch mt-8">
           {experiences.map((item, index) => (
             <ExprienceCard
+              key={index}
+              index={index}
               tanggal={item.tanggal}
               keterangan={item.keterangan}
-              key={index}
               isSelected={selectedIndex === index}
               progress={progressStates[index]}
               onClick={() => handleCardClick(index)}
@@ -69,9 +71,15 @@ const Experiences = () => {
           ))}
         </div>
         <div className="flex w-full justify-center mt-12">
-          <div className="p-8 w-full sm:w-fit bg-primaryBlack-0 rounded-lg shadow-[0_0_15px_5px] shadow-primaryOrange-0">
+          <motion.div
+            initial={{ opacity: 0, y: 75 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            viewport={{ once: true }}
+            className="p-8 w-full sm:w-fit bg-primaryBlack-0 rounded-lg shadow-[0_0_15px_5px] shadow-primaryOrange-0"
+          >
             <GitHubCalendar username="farisfian06" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
