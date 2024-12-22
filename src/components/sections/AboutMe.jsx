3592261b7a3c6abe271d822c1dd4f0526a04d830
farisfian06 <farisfian06@gmail.com>
2024-12-22 20:01:14 +0700

    Update AboutMe.jsx

--- src/components/sections/AboutMe.jsx
+++ src/components/sections/AboutMe.jsx
@@ -1,20 +1,33 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 const AboutMe = () => {
   return (
-    <section className="relative z-10 bg-primaryBlack-0 py-11">
+    <section className="relative z-10 bg-primaryBlack-0 py-11" id="about">
       <div className="container">
         <div className="flex flex-col sm:flex-row sm:justify-between w-full sm:items-center">
-          <div className="sm:w-[50%]">
-            <h2 className="font-primaryBold text-3xl text-primaryOrange-0">
+          <motion.div
+            initial={{ x: -40, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            viewport={{ margin: "-200px", once: true }}
+            transition={{ duration: 0.4 }}
+            className="sm:w-[50%]"
+          >
+            <h2 className="font-primaryBold text-3xl text-primaryOrange-  0">
               ABOUT ME
             </h2>
             <h2 className="font-SerifRegular text-4xl">FARIS IHSAN ALIFIAN</h2>
-          </div>
-          <div className="flex sm:w-[50%] flex-col sm:flex-row h-full sm:h-auto">
+          </motion.div>
+          <motion.div
+            initial={{ x: 40, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            viewport={{ margin: "-400px", once: true }}
+            transition={{ duration: 0.4 }}
+            className="flex sm:w-[50%] flex-col sm:flex-row h-full sm:h-auto"
+          >
             <div className="h-1 self-end w-[80%] rounded bg-primaryOrange-0 mt-4 sm:hidden"></div>
             <div className="min-h-full w-1 rounded bg-primaryOrange-0 me-4 hidden sm:block"></div>
-            <p className="font-primaryRegular w-full text-justify sm:w-[90%] text-base sm:text-sm py-3 mt-2">
+            <p className="font-primaryRegular w-full text-justify sm:w-[90%] text-base py-3 mt-2">
               I am a undergraduate Student of Information System Study, Faculty
               of Computer Science, University of Brawijaya, who has expertise in
               front end website development. Has expertise in HTML, CSS,
@@ -24,7 +37,7 @@ const AboutMe = () => {
               skills. Always learning and adapting to new technologies and
               focusing on career development in the professional field.
             </p>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
