603f92717b76da37bc0bbf9e39a8871588facc9f
farisfian06 <farisfian06@gmail.com>
2024-12-25 22:40:47 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { IoMdHome } from "react-icons/io";
 
 const Navbar = () => {
   const [isHidden, setIsHidden] = useState(false);
@@ -37,6 +38,13 @@ const Navbar = () => {
       className="fixed top-0 z-50 flex w-full justify-center pt-3"
     >
       <ul className="flex justify-between gap-3 rounded-3xl font-primaryBold text-primaryBlack-0 bg-white p-5 *:rounded-xl *:border *:border-gray-200 *:px-3 sm:*:px-7 *:py-2 *:transition-colors *:duration-300 hover:*:bg-gray-200 focus-visible:*:bg-gray-200">
+        <Link
+          className="flex justify-center items-center"
+          to="/"
+          onClick={() => scrollToSection("hero")}
+        >
+          <IoMdHome size={20} />
+        </Link>
         <Link to="/" onClick={() => scrollToSection("about")}>
           About
         </Link>
@@ -50,22 +58,6 @@ const Navbar = () => {
           Tech
         </Link>
       </ul>
-
-      {/* <a href="#" className="bg-gray-200">
-          <svg
-            className="h-6 w-6"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5-10-5-10 5z"></path>
-          </svg>
-          <span className="sr-only">Home</span>
-        </a>
-        <a href="#">Products</a>
-        <a href="#">Services</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a> */}
     </motion.nav>
   );
 };
